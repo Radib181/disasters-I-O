6215b28f64101b74e7843ee96b56d333b0a55b65
First-project <mehedihasan0937580@gmail.com>
2024-10-16 05:00:57 +0600

    Dashboard page done

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -1,20 +1,32 @@
-import { CardDetails } from '../../lib/db';
-import Filter from './../../shared/Filter';
-import Card from './Card';
-
+import { useState } from "react";
+import { CardDetails } from "../../lib/db";
+import Filter from "./../../shared/Filter";
+import Card from "./Card";
+import { IoClose } from "react-icons/io5";
+import ChatBox from "./ChatBox";
 
 const Dashboard = () => {
+  const [chatOpen, setChatOpen] = useState(false);
+  const handleChat = () => {
+    setChatOpen(!chatOpen);
+  };
   return (
     <div>
       <Filter welcome="welcome back" heading="Dashboard" btnText="Cypher AI" />
-      <div className="grid grid-cols-4 gap-2 p-4">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-2 p-4">
         {CardDetails.map((cardDetails, i) => (
           <Card key={i} {...cardDetails} />
         ))}
       </div>
-      <button className="bg-orange text-white w-[77px] h-[77px] rounded-full inline-flex justify-center items-center bottom-4 right-4 fixed hover:cursor-pointer font-bold text-[55px] font-white font-babesNeue">
-        C
+
+      <button
+        className="bg-orange text-white md:w-[77px] w-[57px] md:h-[77px] h-[57px] rounded-full inline-flex justify-center items-center bottom-4 right-4 fixed hover:cursor-pointer font-bold text-[55px] font-white font-babesNeue z-[9999]"
+        onClick={handleChat}
+      >
+        {chatOpen ? <IoClose /> : "C"}
       </button>
+      {/* chatBox component */}
+      <div className="fixed top-0 right-4">{chatOpen ? <ChatBox /> : null}</div>
     </div>
   );
 };
--- src/components/shared/Filter.jsx
+++ src/components/shared/Filter.jsx
@@ -1,9 +1,9 @@
-import Button from "../common/Button"
+import Button from "../common/Button";
 import { IoIosSearch } from "react-icons/io";
 
-const Filter = ({welcome, heading, btnText}) => {
+const Filter = ({ welcome, heading, btnText }) => {
   return (
-    <section className="bg-whiteLight py-6">
+    <section className="bg-whiteLight lg:py-6 py-3">
       <div className="flex md:flex-row flex-col justify-between items-center mx-20">
         <div>
           <p className=" text-[12px] text-grayBold">{welcome}</p>
@@ -11,26 +11,26 @@ const Filter = ({welcome, heading, btnText}) => {
         </div>
         <div>
           <form className="flex md:flex-row flex-col gap-3">
-              <div className="flex relative">
-                <input
-                  type="text"
-                  placeholder="Search incident"
-                  className="py-2 px-6 rounded-[6px] text-[12px] text-grayBold w-40 md:w-48 h-10"
-                />
-                <IoIosSearch className=" absolute top-[10px] left-1 text-zinc-500 h-5 w-5" />
-              </div>
-
+            <div className="flex relative">
               <input
                 type="text"
-                placeholder="sort by: Date modified"
-                className="md:w-48 w-40 h-10 py-2 px-6 rounded-[6px] text-[12px] text-grayBold"
+                placeholder="Search incident"
+                className="py-2 px-6 rounded-[6px] text-[12px] text-grayBold w-40 md:w-48 h-10"
               />
+              <IoIosSearch className=" absolute top-[10px] left-1 text-zinc-500 h-5 w-5" />
+            </div>
+
+            <input
+              type="text"
+              placeholder="sort by: Date modified"
+              className="md:w-48 w-40 h-10 py-2 px-6 rounded-[6px] text-[12px] text-grayBold"
+            />
             <Button className="mx-auto">{btnText}</Button>
           </form>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Filter
+export default Filter;
