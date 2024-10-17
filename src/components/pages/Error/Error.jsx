104609a964e2a776ffa1e7b0801c7908eaf14545
First-project <mehedihasan0937580@gmail.com>
2024-10-18 00:53:59 +0600

    error page responsive

--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -10,11 +10,11 @@ const Error = () => {
     <>
       <div className="flex flex-col gap-5 justify-center items-center h-[85vh]">
         <MdOutlineClose className="w-16 h-16 p-3 bg-red-500 text-white rounded-full" />
-        <h1 className="font-bold font-babesNeue text-5xl tracking-[10px]">
+        <h1 className="font-bold font-babesNeue md:text-5xl text-2xl text-center md:tracking-[10px] tracking-[5px]">
           Data Not Available
         </h1>
         <Button
-          className="text-4xl w-1/4 px-3 bg-sky-400 h-16 rounded-2xl"
+          className="md:text-4xl text-2xl lg:w-1/4 w-1/2 px-3 bg-sky-400 h-16 rounded-2xl"
           handleChange={back}
         >
           Back Home
