52c519eb6f0daa48be2713ae71b06a1a6d5735e2
First-project <mehedihasan0937580@gmail.com>
2024-10-19 20:33:56 +0600

    incidents page remove checking onclick

--- src/components/pages/incidents/Incidents.jsx
+++ src/components/pages/incidents/Incidents.jsx
@@ -3,16 +3,12 @@ import Filter from "../../shared/Filter";
 import Card from "../dashboard/Card";
 
 const Incidents = () => {
-  const handle = () => {
-    console.log("hello world");
-  };
   return (
     <div>
       <Filter
         welcome="Home - Incidents"
         heading="Incidents"
         btnText="+ New Incidents"
-        onclick={handle}
       />
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3 lg:p-6 p-3">
         {CardDetails.map((cardDetails, i) => (
